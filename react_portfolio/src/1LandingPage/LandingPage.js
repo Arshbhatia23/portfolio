020f1229d399ac1a675d7fde1550dc1642992381
Arshbhatia23 <arshbhatia2311@gmail.com>
2023-08-19 23:07:55 +0530

    landing page removed main and div. also fixed height of page as 100vh

--- react_portfolio/src/1LandingPage/LandingPage.js
+++ react_portfolio/src/1LandingPage/LandingPage.js
@@ -3,21 +3,15 @@ import data from './../data.json';
 
 export default function LandingPage() {
   return (
-    <div>
-      <main>
-        
-        <div className={styles.landing_page_container}> 
-        <div className={styles.titleName}>
-             <h1 className={styles.name}>{data.firstName} {data.lastName}</h1>
-        </div>
-      <div className={styles.titleDesc}>
-        <h4>{data.home_desc_1} <br /> 
-        {data.home_desc_2}<br /> 
-        {data.home_desc_3}</h4>
+    <div className={styles.landing_page_container}>
+      <div className={styles.titleName}>
+        <h1 className={styles.name}>{data.firstName} {data.lastName}</h1>
       </div>
-         </div>
-         
-      </main>
+      <div className={styles.titleDesc}>
+        <h4>{data.home_desc_1} <br />
+          {data.home_desc_2}<br />
+          {data.home_desc_3}</h4>
+      </div>
     </div>
   )
 }
