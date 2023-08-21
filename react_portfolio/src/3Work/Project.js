5e9606df50a932fa7e9cd1fabd331594cf2679fb
LAPTOP-V95RNOD1\SHUBHI <shubhiguptahtc@gmail.com>
2023-08-21 23:11:43 +0530

    added carousel button images

--- react_portfolio/src/3Work/Project.js
+++ react_portfolio/src/3Work/Project.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 import styles from './Work.module.css';
 import arrow from '../Images/caret-right-solid.svg'
 import dineIn from '../Images/dineIn.jpeg'
+import dineInIphone from '../Images/iphone-11-mockup.png'
 
 
 export default function Project({workDetail}){
 
      const [scrolled,setScrolled] = useState(false);
+
+     function getImage(){
+        if(workDetail.mockImage=='1'){
+            return dineIn;
+        }else return dineInIphone;
+     }
 
     return(
         <div className={styles.mainContainer}>
@@ -20,9 +27,10 @@ export default function Project({workDetail}){
             <img src={arrow} className={styles.logo} onClick={()=> setScrolled(!scrolled)}/>
             </div>
             </>
-            : <div className={styles.mainContainer}>
+            : <div className={styles.mainContainer} style={{backgroundColor:'#E0D1A9'}}>
                  <div className={styles.workContainer2}> 
-                 <image src={dineIn} className={styles.img1}></image>
+                 {/* <img src={dineIn} className={styles.img1}/> */}
+                 <img src={getImage()} className={styles.img1}/>
                  </div>
             <div>
             <img src={arrow} className={styles.logo} onClick={()=> setScrolled(!scrolled)}/>
