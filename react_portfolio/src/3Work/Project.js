bdd0aad5f82857d4413b930151b4284acb736155
LAPTOP-V95RNOD1\SHUBHI <shubhiguptahtc@gmail.com>
2023-08-21 22:55:06 +0530

    added carousel button

--- react_portfolio/src/3Work/Project.js
+++ react_portfolio/src/3Work/Project.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Work.module.css';
+import arrow from '../Images/caret-right-solid.svg'
+import dineIn from '../Images/dineIn.jpeg'
+
 
 export default function Project({workDetail}){
+
+     const [scrolled,setScrolled] = useState(false);
+
     return(
-        <div className={styles.workContainer}>
-            <main>
+        <div className={styles.mainContainer}>
+            { !scrolled ? 
+            <>
+            <div className={styles.workContainer2}> 
             <h1>{workDetail.projectName}</h1>
             <p className={styles.description}>{workDetail.projectDesc}</p>
-            </main>
+            </div>
+            <div>
+            <img src={arrow} className={styles.logo} onClick={()=> setScrolled(!scrolled)}/>
+            </div>
+            </>
+            : <div className={styles.mainContainer}>
+                 <div className={styles.workContainer2}> 
+                 <image src={dineIn} className={styles.img1}></image>
+                 </div>
+            <div>
+            <img src={arrow} className={styles.logo} onClick={()=> setScrolled(!scrolled)}/>
+            </div>
+                </div>}
         </div>
     )
 }
